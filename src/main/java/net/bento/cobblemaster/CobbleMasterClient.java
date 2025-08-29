bab8990240db9ef1bb998e3ec00c40818835c21c
Bento <pminhkhoi2409@gmail.com>
2025-08-29 10:07:54 +0700

    added source

---
+++ src/main/java/net/bento/cobblemaster/CobbleMasterClient.java
@@ -0,0 +1,10 @@
+package net.bento.cobblemaster;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobbleMasterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
